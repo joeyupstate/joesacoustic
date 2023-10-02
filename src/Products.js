73b853a990740257255c719062f7b6b9c84f384c
joe <jabele@coalmarch.com>
2023-10-02 18:09:55 -0400

    test2

---
+++ src/Products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/1.jpeg";
+import product2 from "./assets/2.jpeg";
+import product3 from "./assets/3.jpeg";
+import product4 from "./assets/4.jpeg";
+import product5 from "./assets/5.jpeg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpeg";
+import product8 from "./assets/8.jpeg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Gibson J-45 Standard",
+    price: 1999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Taylor DS12-M",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Taylor GS Mini-e Koa",
+    price: 800.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Martin DX122-M",
+    price: 1229.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Taylor Surfbird",
+    price: 590.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Collings 122F",
+    price: 2006.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Martin WXL-12",
+    price: 1300.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Taylor 814ce-N ",
+    price: 2999.0,
+    productImage: product8,
+  },
+];
